ba35651f16fd09d92efafe6b036c710418be3911
Fabricio Machado <fabrizio.machado@tup-take.com>
2017-03-19 13:06:31 -0400

    PB: initial scaffolding for point buddy register web app with basic login screen.

---
+++ src/components/PBLogin.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- root node required -->
+  <div>
+    <!-- your content -->
+    <div class="layout-padding">
+      <!-- if you want automatic padding -->
+      <div class="list">
+        <div class="item two-lines">
+          <div style="margin-right: 10px;" class="item-label">username:</div>
+          <div class="item-content">
+            <input placeholder="username" class="full-width">
+          </div>
+        </div>
+        <hr>
+        <div class="item two-lines">
+          <div style="margin-right: 10px;" class="item-label">password:</div>
+          <div class="item-content">
+            <input placeholder="password" class="full-width">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {}
+    }
+  }
+
+</script>
+
+<style>
+
+
+</style>
