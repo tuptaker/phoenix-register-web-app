dc0f12dcb3b07401cd338865943a9e821b4ae1c4
Fabricio Machado <fabrizio.machado@tup-take.com>
2017-06-18 16:38:53 -0400

    Wire axios network call to register REST method

--- src/components/PBRegister.vue
+++ src/components/PBRegister.vue
@@ -4,26 +4,6 @@
     <!-- your content -->
     <div class="layout-padding">
       <!-- if you want automatic padding -->
-    </div>
-  </div>
-</template>
-
-<script>
-export default {
-  data () {
-    return {}
-  }
-}
-</script>
-
-<style>
-</style>
-<template>
-  <!-- root node required -->
-  <div>
-    <!-- your content -->
-    <div class="layout-padding">
-      <!-- if you want automatic padding -->
             <p class="text-center">Enter your email, desired username, and password below to create an account.</p>
       <p class="text-center">Or tap the 'Login' tab above to login if you already have an account.</p>
       <div class="list">
@@ -48,10 +28,38 @@ export default {
           </div>
         </div>
       </div>
-      <button class="primary full-width">Create Account</button>
+      <button class="primary full-width" v-on:click="registerUser('blah')">Create Account</button>
     </div>
   </div>
 </template>
+
+<script>
+
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:4343'
+
+export default {
+  data () {
+    return {}
+  },
+  methods: {
+    registerUser: function (msg) {
+      console.log(msg)
+      axios.post('/register', {
+        username: 'testname',
+        password: 'testpw'
+      }).then(function (response) {
+        console.log(response)
+      }).catch(function (err) {
+        console.log(err)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
 
 <style>
 </style>
